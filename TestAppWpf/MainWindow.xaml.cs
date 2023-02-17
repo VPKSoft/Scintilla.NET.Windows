92a9d87862e7a1ead07d31c01a5cf05577131cce
VPKSoft <vpksoft@vpksoft.net>
2023-02-17 19:37:20 +0200

    Unsaved changes.

--- TestAppWpf/MainWindow.xaml.cs
+++ TestAppWpf/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TestAppWpf;
 /// <summary>
@@ -22,5 +10,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private bool shown;
+
+    private void MainWindow_OnContentRendered(object? sender, EventArgs e)
+    {
+        if (IsVisible && !shown)
+        {
+            shown = true;
+        }
     }
 }
